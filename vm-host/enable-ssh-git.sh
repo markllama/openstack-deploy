98abc350e8a02587418078bdce609a164eb98271
Mark Lamourine <markllama@gmail.com>
2020-11-09 18:22:22 -0500

    prepare the lab host for logins and configuration

---
+++ vm-host/enable-ssh-git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ssh-keygen -R lab
+ssh-copy-id -o PreferredAuthentications=password lab
+scp ~/.ssh/markllama@gmail.com_rsa lab:.ssh
+grep -A1 github.com ~/.ssh/config | ssh lab tee -a .ssh/config
+ssh lab chmod 600 .ssh/*
+cat .gitconfig | ssh lab tee .gitconfig
