f26de9e84a64d7b5f8600364d5db86e371f498e7
Mark Lamourine <markllama@gmail.com>
2020-11-11 11:09:29 -0500

    try just using nmcli for nets

--- vm-host/enable-ssh-git.sh
+++ vm-host/enable-ssh-git.sh
@@ -5,4 +5,4 @@ ssh-copy-id -o PreferredAuthentications=password lab
 scp ~/.ssh/markllama@gmail.com_rsa lab:.ssh
 grep -A1 github.com ~/.ssh/config | ssh lab tee -a .ssh/config
 ssh lab chmod 600 .ssh/*
-cat .gitconfig | ssh lab tee .gitconfig
+cat ~/.gitconfig | ssh lab tee .gitconfig
