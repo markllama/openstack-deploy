17392e4c8590482fe7505e5e9be179612395a59f
Mark Lamourine <markllama@gmail.com>
2020-11-12 21:38:33 -0500

    dont att stack user to the vm-host, and default remote user to stack on the vm-host

--- vm-host/enable-ssh-git.sh
+++ vm-host/enable-ssh-git.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ : ${REMOTE_USER:=stack}
+
 ssh-keygen -R lab
-ssh-copy-id -o PreferredAuthentications=password lab
-scp ~/.ssh/markllama@gmail.com_rsa lab:.ssh
-grep -A1 github.com ~/.ssh/config | ssh lab tee -a .ssh/config
-ssh lab chmod 600 .ssh/*
-cat ~/.gitconfig | ssh lab tee .gitconfig
+ssh-copy-id -o PreferredAuthentications=password ${REMOTE_USER}@lab
+scp ~/.ssh/markllama@gmail.com_rsa ${REMOTE_USER}@lab:.ssh
+grep -A1 github.com ~/.ssh/config | ssh ${REMOTE_USER}@lab tee -a .ssh/config
+ssh ${REMOTE_USER}@lab chmod 600 .ssh/*
+cat ~/.gitconfig | ssh ${REMOTE_USER}@lab tee .gitconfig
