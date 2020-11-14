3be8d0f5ae6b24e9d243d65585daf340fb6f3ef5
Mark Lamourine <markllama@gmail.com>
2020-11-14 14:32:05 +0000

    removed other parts from enable

--- vm/enable-ssh-git.sh
+++ vm/enable-ssh-git.sh
@@ -7,5 +7,5 @@ ssh-keygen -R ${REMOTE_HOST}
 ssh-copy-id -o PreferredAuthentications=password ${REMOTE_USER}@${REMOTE_HOST}
 scp ~/.ssh/markllama@gmail.com_rsa ${REMOTE_USER}@${REMOTE_HOST}:.ssh
 grep -A1 github.com ~/.ssh/config | ssh ${REMOTE_USER}@${REMOTE_HOST} tee -a .ssh/config
-ssh ${REMOTE_USER}@${REMOTE_HOST} chmod 600 .ssh/*
+ssh ${REMOTE_USER}@${REMOTE_HOST} chmod 600 .ssh/\*
 cat ~/.gitconfig | ssh ${REMOTE_USER}@${REMOTE_HOST} tee .gitconfig
