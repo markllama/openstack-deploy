e7e1fa9db632140c07db3b65de0a3d1962d5b3c6
Mark Lamourine <markllama@gmail.com>
2020-11-15 04:10:46 +0000

    add download and upload script for centos images

---
+++ stack/scripts/install_overcloud_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+: ${REPO_URL:="https://images.rdoproject.org/centos7/queens/rdo_trunk/current-tripleo-rdo/"}
+: ${IMAGE_DIR:=~/images}
+
+DOWNLOAD_FILES=(
+    ironic-python-agent.tar
+    overcloud-full.tar
+    undercloud.qcow2
+)
+
+for FILE in ${DOWNLOAD_FILES[@]} ; do
+    [ -f ${REPO_URL}${FILE} ] || wget -O ${IMAGE_DIR}/${FILE} ${REPO_URL}${FILE}
+    [ -f ${REPO_URL}${FILE}.mp3 || wget -O ${IMAGE_DIR}/${FILE}.md5 ${REPO_URL}${FILE}.md5
+done
+
+for TAR in ${IMAGE_DIR}/*.tar ; do
+    tar -xf ${TAR}
+done
+
+
+
+
