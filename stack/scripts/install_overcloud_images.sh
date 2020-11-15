98665bae828a18d8673d4036298b4f6e0436db6c
Mark Lamourine <markllama@gmail.com>
2020-11-15 21:31:49 +0000

    switch -O to -o for curl

--- stack/scripts/install_overcloud_images.sh
+++ stack/scripts/install_overcloud_images.sh
@@ -9,11 +9,24 @@ DOWNLOAD_FILES=(
     undercloud.qcow2
 )
 
+mkdir -p ${IMAGE_DIR}
+
 for FILE in ${DOWNLOAD_FILES[@]} ; do
-    [ -f ${REPO_URL}${FILE} ] || curl -O ${IMAGE_DIR}/${FILE} ${REPO_URL}${FILE}
-    [ -f ${REPO_URL}${FILE}.mp3 ] || curl -O ${IMAGE_DIR}/${FILE}.md5 ${REPO_URL}${FILE}.md5
+    [ -f ${REPO_URL}${FILE} ] || curl -o ${IMAGE_DIR}/${FILE} ${REPO_URL}${FILE}
+    [ -f ${REPO_URL}${FILE}.mp3 ] || curl -o ${IMAGE_DIR}/${FILE}.md5 ${REPO_URL}${FILE}.md5
 done
 
 for TAR in ${IMAGE_DIR}/*.tar ; do
-    tar -xf ${TAR}
+    tar -C ${IMAGE_DIR} -xf ${TAR}
 done
+
+openstack overcloud image upload
+
+#declare -A IMAGE_FILES=(
+#    ['overcloud-full-vmlinuz']=overcloud-full.vmlinuz
+#    ['overcloud-full-initrd']=overcloud-full.initrd
+#    ['overcloud-full']=overcloud-full.qcow2
+#    ['bm-deploy-kernel']=ironic-python-agent.kernel
+#    ['bm-deploy-ramdisk']=ironic-python-agent.initramfs
+#)
+
