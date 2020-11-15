0badf71757949853d0b2c02a3c733f956363aa80
Mark Lamourine <markllama@gmail.com>
2020-11-15 04:41:45 +0000

    no wget - use curl

--- stack/scripts/install_overcloud_images.sh
+++ stack/scripts/install_overcloud_images.sh
@@ -10,8 +10,8 @@ DOWNLOAD_FILES=(
 )
 
 for FILE in ${DOWNLOAD_FILES[@]} ; do
-    [ -f ${REPO_URL}${FILE} ] || wget -O ${IMAGE_DIR}/${FILE} ${REPO_URL}${FILE}
-    [ -f ${REPO_URL}${FILE}.mp3 || wget -O ${IMAGE_DIR}/${FILE}.md5 ${REPO_URL}${FILE}.md5
+    [ -f ${REPO_URL}${FILE} ] || curl -O ${IMAGE_DIR}/${FILE} ${REPO_URL}${FILE}
+    [ -f ${REPO_URL}${FILE}.mp3 ] || curl -O ${IMAGE_DIR}/${FILE}.md5 ${REPO_URL}${FILE}.md5
 done
 
 for TAR in ${IMAGE_DIR}/*.tar ; do
